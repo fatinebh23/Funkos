ffb28b159b962584abf9ff2df562156e5386c43f
daw2 <fatinebh23@gmail.com>
2023-11-17 10:07:03 +0100

    Initial commit

---
+++ src/main/java/org/example/Funko.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Funko {
+    private String cod;
+    private String nombre;
+    private String modelo;
+
+    private double precio;
+    private LocalDate fecha_lanzamiento;
+
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,57 @@
+package org.example;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+import java.nio.file.Path;
+
+public class Main {
+    private final static String COMMA_DELIMITER = ",";
+
+    public static void main(String[] args) throws IOException {
+
+
+        try (Stream<String> contenidoFichero = Files.lines(Path.of(".", "src", "main", "resources", "funkos.csv"))) {
+            List<List<String>> funkosString = contenidoFichero.map(l -> Arrays.asList(l.split(COMMA_DELIMITER))).toList();
+            //funkos.forEach(System.out::println);
+            List<Funko> funkosFunkos = new ArrayList<>();
+
+
+
+
+
+            for (int i = 1; i < funkosString.size(); i++) {
+                List<String> unFunko = funkosString.get(i);
+
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+
+                Funko ejemplo = new Funko();
+                ejemplo.setCod(unFunko.get(0));
+                ejemplo.setNombre(unFunko.get(1));
+                ejemplo.setModelo(unFunko.get(2));
+                ejemplo.setPrecio(Double.parseDouble(unFunko.get(3)));
+                ejemplo.setFecha_lanzamiento(LocalDate.parse(unFunko.get(4), formatter));
+
+                funkosFunkos.add(ejemplo);
+            }
+
+
+            funkosFunkos.forEach(System.out::println);
+
+       double funkoCaro= funkosFunkos.stream()
+                .map(funko -> funko.getPrecio)
+                .max(double::compareTo)
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace(System.out);
+        }
+    }
+}
